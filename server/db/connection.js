e8f037efeea770a5796dd8f2358dae309fc8f4cb
Harris Jayaram <harrisjayaramr@gmail.com>
2024-06-26 19:18:08 +0530

    ChatApp

---
+++ server/db/connection.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const url = 'mongodb+srv://chat_app_admin:12345@cluster0.iic3kbk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+
+}).then(()=>console.log('Connected to DB')).catch((e)=>console.log('Error connecting', e));
